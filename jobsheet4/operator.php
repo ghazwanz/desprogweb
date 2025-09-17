d9f69cd61e92ba2307afc00eec7c359b459fdbec
ghazwanz <ababilghazwan@gmail.com>
2025-09-17 23:20:55 +0700

    Jobsheet 4 Praktikum 3 Penggunaan Operator PHP 2 Operator Perbandingan

--- jobsheet4/operator.php
+++ jobsheet4/operator.php
@@ -14,4 +14,23 @@ echo "Hasil dari Perkalian $a * $b adalah $hasilKali <br>";
 echo "Hasil dari Pembagian $a / $b adalah $hasilBagi <br>";
 echo "Hasil dari modulus (sisa bagi) $a % $b adalah $sisaBagi <br>";
 echo "Hasil Perpangkatan dari $a<sup>$b</sup> adalah $pangkat <br>";
+
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
+
+function checkBoolean ($args){
+    return $args ? "benar" : "salah";
+};
+
+echo "<br>";
+echo "Perbandingan dari sama dengan $a == $b adalah <b>" . checkBoolean($hasilSama) . "</b><br>";
+echo "Perbandingan dari tidak sama dengan $a != $b adalah <b>" . checkBoolean($hasilTidakSama) . "</b><br>";
+echo "Perbandingan dari lebih kecil $a < $b adalah <b>" . checkBoolean($hasilLebihKecil) . "</b><br>";
+echo "Perbandingan dari lebih besar $a > $b adalah <b>" . checkBoolean($hasilLebihBesar) . "</b><br>";
+echo "Perbandingan dari lebih kecil sama dengan $a <= $b adalah <b>" . checkBoolean($hasilLebihKecilSama) . "</b><br>";
+echo "Perbandingan dari lebih besar sama dengan $a >= $b adalah <b>" . checkBoolean($hasilLebihBesarSama) . "</b><br>";
 ?>
