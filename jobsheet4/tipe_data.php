4aa78e5a00e0dbb9f0f2ff2b3328f162b9681cd7
ghazwanz <ababilghazwan@gmail.com>
2025-09-17 21:42:41 +0700

    Jobsheet 4 Praktikum 2 Penggunaan Tipe Data

---
+++ jobsheet4/tipe_data.php
@@ -0,0 +1,33 @@
+<?php
+$a = 10;
+$b = 5;
+$c = $a + 5;
+$d = $b + (10 * 5);
+$e = $d - $c;
+
+echo "Variabel a: {$a} <br>";
+echo "Variabel b: {$b} <br>";
+echo "Variabel c: {$c} <br>";
+echo "Variabel d: {$d} <br>";
+echo "Variabel e: {$e} <br>";
+
+var_dump($e);
+echo "<br>";
+$apakahSiswaLulus = true;
+$apakahSiswaSudahUjian = false;
+
+var_dump($apakahSiswaLulus);
+echo "<br>";
+var_dump($apakahSiswaSudahUjian);
+
+$namaDepan = "Ibnu";
+$namaBelakang = "Jakaria";
+
+$namaLengkap = "{$namaDepan} {$namaBelakang}";
+$namaLengkap2 = $namaDepan . " " . $namaBelakang;
+
+echo "<br>" . $namaLengkap . "<br>";
+
+$listMahasiswa = ["Wahid Abdullah","Elmo Bachtiar","Lendis Fabri"];
+echo $listMahasiswa[0];
+?>
