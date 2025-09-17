8038e0f9d4d83afc2fead375fb1e8b6fb90da69c
ghazwanz <ababilghazwan@gmail.com>
2025-09-17 21:59:29 +0700

    Jobsheet 4 Praktikum 3 Penggunaan Operator PHP 1 Operator Aritmatika

---
+++ jobsheet4/operator.php
@@ -0,0 +1,17 @@
+<?php
+$a = 10;
+$b = 5;
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$sisaBagi = $a % $b;
+$pangkat = $a ** $b;
+
+echo "Hasil dari Penjumlahan $a + $b adalah $hasilTambah <br>";
+echo "Hasil dari Pengurangan $a - $b adalah $hasilKurang <br>";
+echo "Hasil dari Perkalian $a * $b adalah $hasilKali <br>";
+echo "Hasil dari Pembagian $a / $b adalah $hasilBagi <br>";
+echo "Hasil dari modulus (sisa bagi) $a % $b adalah $sisaBagi <br>";
+echo "Hasil Perpangkatan dari $a<sup>$b</sup> adalah $pangkat <br>";
+?>
