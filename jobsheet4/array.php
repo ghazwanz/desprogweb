fd122afe6ea8e8390adce9058d3d0741ace4f990
ghazwanz <ababilghazwan@gmail.com>
2025-09-20 23:57:07 +0700

    Jobsheet 4 Praktikum 5 Penggunaan Array Pada PHP Bagian 3 Array Asosiatif

--- jobsheet4/array.php
+++ jobsheet4/array.php
@@ -24,4 +24,31 @@ foreach ($daftarkaryawan as $karyawan) {
 }
 echo "<br>";
 echo "Daftar karyawan dengan pengalaman kerja lebih dari 5 tahun: " . implode(', ',$karyawanPengalamanLimaTahun);
+
+$daftarNilai = [
+    'Matematika' => [
+        ['Alice', 85],
+        ['Bob', 92],
+        ['Charlie', 78],
+    ],
+    'Fisika' => [
+        ['Alice', 90],
+        ['Bob', 88],
+        ['Charlie', 75],
+    ],
+    'Kimia' => [
+        ['Alice', 92],
+        ['Bob', 80],
+        ['Charlie', 85],
+    ],
+];
+
+$mataKuliah = 'Fisika';
+
+echo "<br><br>";
+echo "Daftar nilai mahasiswa dalam mata kuliah <b>$mataKuliah</b>: <br>";
+
+foreach ($daftarNilai[$mataKuliah] as $nilai) {
+    echo "Nama: {$nilai [0]}, Nilai: {$nilai[1]} <br>";
+}
 ?>
