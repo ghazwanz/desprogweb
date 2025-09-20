49761556b17f68029cce3ec558ef17e18d058a82
ghazwanz <ababilghazwan@gmail.com>
2025-09-20 23:23:50 +0700

    Jobsheet 4 Praktikum 5 Penggunaan Array Pada PHP Bagian 2 Array Dua Dimensi

--- jobsheet4/array.php
+++ jobsheet4/array.php
@@ -8,4 +8,20 @@ foreach ($nilaiMahasiswa as $nilai) {
 }
 
 echo "Daftar nilai siswa yang lulus: " . implode(", ", $nilaiLulus);
+
+$daftarkaryawan = [
+    ['Alice', 7],
+    ['Bob', 3],
+    ['Charlie', 9],
+    ['David', 5],
+    ['Eva', 6],
+];
+$karyawanPengalamanLimaTahun = [];
+foreach ($daftarkaryawan as $karyawan) {
+    if ($karyawan [1] > 5) {
+        $karyawanPengalamanLimaTahun[] = $karyawan [0];
+    }
+}
+echo "<br>";
+echo "Daftar karyawan dengan pengalaman kerja lebih dari 5 tahun: " . implode(',',$karyawanPengalamanLimaTahun);
 ?>
