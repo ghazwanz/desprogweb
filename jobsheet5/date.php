2c0ff664b0c5128efd00924b21cc1234accdd6b2
ghazwanz <ababilghazwan@gmail.com>
2025-09-29 17:47:01 +0700

    Jobsheet 5 Praktikum 8 HTML dan PHP Cara 2

--- jobsheet5/date.php
+++ jobsheet5/date.php
@@ -2,7 +2,7 @@
     echo '<html>';
     echo '<head><title>Cara02</title></head>';
     echo '<body>';
-    echo '<p>Tanggal Hari ini'.date('d MY').'</p>';
+    echo '<p>Tanggal Hari ini '.date('d MY').'</p>';
     echo '</body>';
     echo '</html>';
 ?>
