33060f19e863dbd5cba7c515cfb7d3f69425dbcf
ghazwanz <ababilghazwan@gmail.com>
2025-09-29 09:49:12 +0700

    Jobsheet 5 Praktikum 4 Fungsi Bagian 2 Fungsi dengan Parameter

--- jobsheet5/function.php
+++ jobsheet5/function.php
@@ -1,10 +1,15 @@
 <?php
-function perkenalan()
-{
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan, nama saya Abil<br/>";
-    echo "Senang berkenalan dengan Anda<br/>";
+//membuat fungsi
+function perkenalan ($nama, $salam) {
+echo $salam . ", ";
+echo "Perkenalkan, nama saya ".$nama."<br/>";
+echo "Senang berkenalan dengan Anda<br/>";
 }
-
-perkenalan();
-perkenalan();
+//memanggil fungsi yang sudah dibuat
+perkenalan ("Hamdana", "Hallo");
+echo "<hr>";
+$saya = "Elok";
+$ucapanSalam = "Selamat pagi";
+//memanggil lagi
+perkenalan ($saya, $ucapanSalam);
+?>
