d67ace6acf51d2265eddede4ec311a9a33d870f3
ghazwanz <ababilghazwan@gmail.com>
2025-09-29 23:02:16 +0700

    Rename file Praktikum 10 Tanggal Bagian 1 Date

--- jobsheet5/date.php
+++ jobsheet5/date.php
@@ -1,8 +1,16 @@
-<?php
-    echo '<html>';
-    echo '<head><title>Cara02</title></head>';
-    echo '<body>';
-    echo '<p>Tanggal Hari ini '.date('d MY').'</p>';
-    echo '</body>';
-    echo '</html>';
-?>
+<!DOCTYPE HTML>
+<html>
+
+<head>
+</head>
+
+<body>
+    <h3> Date </h3>
+    <?php
+    echo "Today is " . date("Y/m/d") . "<br>";
+    echo "Today is " . date("Y.m.d") . "<br>";
+    echo "Today is " . date("Y-m-d") . "<br>";
+    echo "Today is " . date("l");
+    ?>
+</body>
+</html>
