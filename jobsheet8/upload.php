d8c07a4f6a86c591c36d09cf0303cdad327ef8d4
ghazwanz <ababilghazwan@gmail.com>
2025-10-27 11:10:13 +0700

    Jobsheet 8 Praktikum 1 Pertanyaan 5

--- jobsheet8/upload.php
+++ jobsheet8/upload.php
@@ -1,13 +1,25 @@
-// File: upload.php (Versi 1)
+// File: upload.php (Versi 2 - Dengan Validasi)
 <?php
 if (isset($_POST["submit"])) {
     $targetdir = "uploads/";
     $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
-    
-    if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
-        echo "File berhasil diunggah.";
+
+    $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
+
+    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+
+    $maxsize = 5 * 1024 * 1024;
+
+    if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
+        if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
+            echo "File berhasil diunggah.";
+            echo "<br><p>Thumbnail:</p>";
+            echo "<img src='$targetfile' width='200'>";
+        } else {
+            echo "Gagal mengunggah file.";
+        }
     } else {
-        echo "Gagal mengunggah file.";
+        echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan";
     }
 }
 ?>
