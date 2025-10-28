bc83e1ef9197956474778307219628176b73bc2e
ghazwanz <ababilghazwan@gmail.com>
2025-10-28 19:03:02 +0700

    Jobsheet 9 Praktikum 2

--- jobsheet9/koneksi.php
+++ jobsheet9/koneksi.php
@@ -10,20 +10,20 @@ if (!$connect) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
 
-$query = "CREATE TABLE user (
-    id INT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL
-)";
+// $query = "CREATE TABLE user (
+//     id INT PRIMARY KEY,
+//     username VARCHAR(50) NOT NULL,
+//     password VARCHAR(50) NOT NULL
+// )";
 
-mysqli_query($connect, $query);
+// mysqli_query($connect, $query);
 
-$id = 1;
-$username = 'admin';
-$password = md5('123');
+// $id = 1;
+// $username = 'admin';
+// $password = md5('123');
 
-$query = "INSERT INTO user (id, username, password) 
-            VALUES ('$id', '$username', '$password')";
+// $query = "INSERT INTO user (id, username, password) 
+//             VALUES ('$id', '$username', '$password')";
 
-mysqli_query($connect, $query);
+// mysqli_query($connect, $query);
 ?>
